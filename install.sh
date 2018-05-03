5686620b71e20987de2aa3c3b0b8f38fb605a26c
Taras I. Ivaniv <tarasii@gmail.com>
2018-05-03 20:02:08 +0300

    pip upgrade

--- install.sh
+++ install.sh
@@ -5,9 +5,8 @@ cd /home/pi/wipeon2
 sudo apt-get update
 sudo apt-get install python-pip
 sudo apt-get install python-dev
-sudo pip install --index-url=https://pypi.python.org/simple/ --upgrade pip
 
-pip install -r requirements.txt
+sudo pip install --index-url=https://pypi.python.org/simple/ -r requirements.txt
 
 sudo apt-get install supervisor
 sudo cp wipeon.conf /etc/supervisor/conf.d/
@@ -17,5 +16,7 @@ sudo apt-get install avahi-daemon
 sudo cp wipeon.service /etc/avahi/services/
 
 sudo usermod -a -G lpadmin pi
+
+sudo pip install --index-url=https://pypi.python.org/simple/ --upgrade pip
 
 #python /home/pi/wipeon2/print_barcode.py buttons
